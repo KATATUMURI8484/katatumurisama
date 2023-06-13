f046ee8e0eebe49144100cd38c086b345c43184d
KATATUMURI8484 <katatumuri8484@gmail.com>
2023-06-13 19:52:43 +0900

    忘れた

--- qwerty.js
+++ qwerty.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function(){
-    var header =doucment.querySelector('header');
-    header.classList.toggle('sticky',window.scrolly > 0);
+window.addEventListener('scroll', function () {
+    var header = doucment.querySelector('header');
+    header.classList.toggle('sticky', window.scrolly > 0);
 });
