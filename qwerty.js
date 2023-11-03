272f9f62f6a236c3e322f2cb6386224a90c4cf7a
KATATUMURI8484 <katatumuri8484@gmail.com>
2023-11-03 23:52:34 +0900

    なんかいろいろ

--- qwerty.js
+++ qwerty.js
@@ -1,3 +1,12 @@
-$(".han").click(function(){
- $(this).toggleclass('active')
-});
+function check(){
+    let form1 = document.getElementById("form1")
+    alert(aleform1.value + "あああああ");
+      }
+      function osu() {
+        let aa = document.getElementById("aa")
+        aa.style.backgroundColor = "#333333";
+        aa.style.color = "white"
+        let bb = document.getElementById("bb")
+        bb.style.backgroundColor = "#eeeff0"
+        bb.style.color = "black"
+      }
