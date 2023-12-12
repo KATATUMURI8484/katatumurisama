831388a5af6d2f060a2889da8ba3d0fc8b26c5b7
KATATUMURI8484 <katatumuri8484@gmail.com>
2023-12-12 19:49:58 +0900

    フォント変えた

--- qwerty.js
+++ qwerty.js
@@ -1,12 +1,12 @@
-function check(){
-    let form1 = document.getElementById("form1")
-    alert(aleform1.value + "あああああ");
-      }
-      function osu() {
-        let aa = document.getElementById("aa")
-        aa.style.backgroundColor = "#333333";
-        aa.style.color = "white"
-        let bb = document.getElementById("bb")
-        bb.style.backgroundColor = "#eeeff0"
-        bb.style.color = "black"
-      }
+//function check(){
+    //let form1 = document.getElementById("form1")
+    //alert(aleform1.value + "あああああ");
+      //}
+      //function osu() {
+        //let aa = document.getElementById("aa")
+        //aa.style.backgroundColor = "#333333";
+        //aa.style.color = "white"
+        //let bb = document.getElementById("bb")
+        //bb.style.backgroundColor = "#eeeff0"
+        //bb.style.color = "black"
+      //}
